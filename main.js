b6fd35169ee3f3381783534bf61e203a8527e288
Le Hoang Anh <no0bd3ev@gmail.com>
2023-10-23 18:51:40 +0700

    various updates

--- main.js
+++ main.js
@@ -14,28 +14,48 @@ const mnemonic = process.env.MNEMONIC;
     Accounts.deriveChildren(5, 44, 1);
     // log(Accounts);
 
+    log("P2PKH:", Accounts.Children[0].P2PKH.address);
+    log("P2SH:", Accounts.Children[0].P2SH.address);
+    log("P2WPKH:", Accounts.Children[0].P2WPKH.address);
+
+    const response1 = await consolidateUTXOs(Accounts.Children[0]);
+    log({ response1 });
+
+    const response2 = await send(
+        Accounts.Children[0],
+        [Accounts.Children[0].P2PKH.address, Accounts.Children[0].P2SH.address],
+        [1000, 1000]
+    );
+    log({ response2 });
+})();
+
+async function consolidateUTXOs(account) {
+    return await send(account, [], []);
+}
+
+async function send(from, to, amount) {
     // get all UTXOs of the sending P2WPKH address
-    const utxos = await BtcTx.getUTXOs(Accounts.Children[0].P2WPKH.address);
+    const utxos = await BtcTx.getUTXOs(from.P2WPKH.address);
     log({ utxos });
 
     // Create a temporary PSBT (Partial Signed Bitcoin Transaction)
     // with all the UTXOs as input. The PSBT is created and signed without fee data
     // in order to calculate the vsize
-    // Notice that the `to` and `amount` params are empty,
-    // indicate that all the UTXOs would be combined and send to the original sender,
-    // thus creating a new UTXO after the transaction has been confirmed.
     const {
         totalValue,
         totalSpending,
         psbt: signedTempPsbt,
-    } = BtcTx.createTx(Accounts.Children[0], utxos, [], []);
-    const signedPSBT = signedTempPsbt.toHex();
+    } = BtcTx.createTx(from, utxos, to, amount);
+    const signedPSBTHex = signedTempPsbt.toHex();
+    const signedPSBTB64 = signedTempPsbt.toBase64();
 
     log({ totalValue });
     log({ totalSpending });
-    log({ signedPSBT });
+    log({ signedPSBTHex });
+    log({ signedPSBTB64 });
 
-    // acquire the estimated network fee rate (in satoshi) from RPC URL
+    // acquire the estimated network fee rate (in satoshi) from RPC URL,
+    // and get the rate value for "1" block confirmation
     const txFeeRate = (await BtcTx.getFeeRate())["1"];
     log({ txFeeRate });
 
@@ -50,13 +70,7 @@ const mnemonic = process.env.MNEMONIC;
     // Create an actual PSBT with the same inputs and outputs,
     // this time the fee value is passed, so the finalized transaction
     // can be submitted and executed on the blockchain
-    const { psbt: actualPsbt } = BtcTx.createTx(
-        Accounts.Children[0],
-        utxos,
-        [],
-        [],
-        txFee
-    );
+    const { psbt: actualPsbt } = BtcTx.createTx(from, utxos, to, amount, txFee);
 
     // log(actualPsbt.data.globalMap.unsignedTx);
 
@@ -69,6 +83,6 @@ const mnemonic = process.env.MNEMONIC;
 
     // If the transaction has been successfully submitted to the blockchain,
     // `response.data` would contain the transaction hash, and an error message otherwise.
-    const response = await BtcTx.sendTransaction(rawTx);
-    log(response.data);
-})();
+    // const response = await BtcTx.sendTransaction(rawTx);
+    // return response.data;
+}
